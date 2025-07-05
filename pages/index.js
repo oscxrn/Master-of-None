ab28488189c0ec98a1c8d451f15e05650a172338
Oscar Nicholls <oscar@OSCAR-BEAST.28320>
2025-07-05 20:59:57 +0100

    Restore homepage

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "2rem", maxWidth: "600px", margin: "0 auto" }}>
+      <h1>My Minimal Notion Blog</h1>
+      <p>This is the homepage. Posts will appear here soon.</p>
+    </main>
+  );
+}
